2fdef660f76b504ed84536c941a36f4b3ab152c6
mgomper <mgomper@avans.nl>
2018-06-08 12:21:27 +0200

    added routing

--- SeeChange-client/src/app/app.module.ts
+++ SeeChange-client/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import {MatButtonModule, MatCheckboxModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     MatButtonModule,
     MatCheckboxModule
   ],
