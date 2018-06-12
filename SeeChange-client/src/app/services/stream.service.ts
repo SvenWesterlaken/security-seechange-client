4c8522efafb12cf317680ab3ce39afd3796c2fe4
mgomper <mgomper@avans.nl>
2018-06-12 14:38:13 +0200

    dummy data added

--- SeeChange-client/src/app/services/stream.service.ts
+++ SeeChange-client/src/app/services/stream.service.ts
@@ -11,17 +11,16 @@ export class StreamService{
 
 
   private stream: Stream;
-  private stream1: Stream;
-  private stream2: Stream;
-  private stream3: Stream;
-  private stream4: Stream;
-  private stream5: Stream;
+  private stream1: Stream = new Stream();
+  private stream2: Stream = new Stream();
+  private stream3: Stream = new Stream();
+  private stream4: Stream = new Stream();
+  private stream5: Stream = new Stream();
   private streams: Stream[];
 
 
 
   constructor() {
-    this.streams.fill(this.stream1);
   }
 
   getStreams() {
@@ -37,6 +36,13 @@ export class StreamService{
     //   });
 
     // this.streams.fill(this.stream1);
+    this.stream1._id = 1;
+    this.stream2._id = 2;
+    this.stream3._id = 3;
+    this.stream4._id = 4;
+    this.stream5._id = 5;
+
+    this.streams = [this.stream1, this.stream2, this.stream3, this.stream4, this.stream5];
     return this.streams;
   }
 
