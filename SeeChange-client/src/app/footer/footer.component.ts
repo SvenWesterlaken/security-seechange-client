8d760543ef9db9df379aaa910ead66853887075a
mgomper <mgomper@avans.nl>
2018-06-12 12:41:00 +0200

    footer fix

--- SeeChange-client/src/app/footer/footer.component.ts
+++ SeeChange-client/src/app/footer/footer.component.ts
@@ -9,48 +9,30 @@ import * as $ from 'jquery';
 export class FooterComponent implements OnInit {
 
   constructor() {
-  //   if ($(document.body).height() < $(window).height()) {
-  //   $('.footer').attr('style', 'position: fixed!important; bottom: 0px;');
-  // }
+    //   if ($(document.body).height() < $(window).height()) {
+    //   $('.footer').attr('style', 'position: fixed!important; bottom: 0px;');
+    // }
 
-    $(window).on('load resize scroll', function() {
-      var f = $('#footer');
-      f.css({position:'static'});
-      if (f.offset().top + f.height() < $(window).height()) {
-        f.css({position:'fixed', bottom:'0', width:'100%'});
-      }
-      else if (f.offset().top + f.height() > $(window).height()) {
-        f.css({position:'static', bottom:'', width:'100%'});
-      }
-    });
+    // $(window).on('load resize scroll', function() {
+    //   var f = $('#footer');
+    //   f.css({position:'static'});
+    //   if (f.offset().top + f.height() < $(window).height()) {
+    //     f.css({position:'fixed', bottom:'0', width:'100%'});
+    //   }
+    //   else if (f.offset().top + f.height() > $(window).height()) {
+    //     f.css({position:'static', bottom:'', width:'100%'});
+    //   }
+    // });
+
+
   }
 
-  ngOnInit() {
-    $(window).on('load resize scroll', function() {
-      var f = $('#footer');
-      f.css({position:'static'});
-      if (f.offset().top + f.height() < $(window).height()) {
-        f.css({position:'fixed', bottom:'0', width:'100%'});
-      }
-      else if (f.offset().top + f.height() > $(window).height()) {
-        f.css({position:'static', bottom:'', width:'100%'});
-      }
-    });
+  ngOnInit(): void {
   }
-
-  ngOnChanges(){
-    $(window).on('load resize scroll', function() {
-      var f = $('#footer');
-      f.css({position:'static'});
-      if (f.offset().top + f.height() < $(window).height()) {
-        f.css({position:'fixed', bottom:'0', width:'100%'});
-      }
-      else if (f.offset().top + f.height() > $(window).height()) {
-        f.css({position:'static', bottom:'', width:'100%'});
-      }
-    });
-  }
-
-
-
 }
+
+
+
+
+
+
